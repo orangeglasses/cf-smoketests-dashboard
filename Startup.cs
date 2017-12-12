da07f792e11e1fc5fe33a9cb338f696ee14fd528
Ronald Wildenberg <rwildenberg@itq.nl>
2017-12-12 16:46:48 +0100

    Removed unused code in Startup class

--- Startup.cs
+++ Startup.cs
@@ -12,20 +12,12 @@ namespace SmokeTestsDashboardServer
 {
     public class Startup
     {
-        private IServiceCollection services;
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
-
             services.AddSingleton<IHostedService, CounterHostedService>();
-
-            this.services = services;
-
-            var serviceNames = this.services.Select(service => service.ServiceType.Name + " (" + service.Lifetime + ")");
-            var s = string.Join("\r\n", serviceNames);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
