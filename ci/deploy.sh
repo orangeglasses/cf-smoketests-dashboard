c71ffea1f8107c1988b0505a75ae195c1e3ae549
Ronald Wildenberg <rwildenberg@itq.nl>
2018-01-03 17:50:22 +0100

    Work-in-progress concourse pipeline (should run elm-make and dotnet publish now)

---
+++ ci/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd ./resource-git-smoketests-dashboard/
+
+# Compile elm code
+elm-make elm\Main.elm elm\View.elm elm\Model.elm elm\AppStyles.elm elm\TestResult\Main.elm --output wwwroot\js\main.js
+
+# Publish dotnet core app
+dotnet publish -c Release -r ubuntu.14.04-x64
+
+# Push to CF
