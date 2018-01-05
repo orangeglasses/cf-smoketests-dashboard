ee6949c67a04f73f7fad19cf0973437370984ac0
Ronald Wildenberg <rwildenberg@itq.nl>
2018-01-05 11:33:03 +0100

    Added actual CF deployment step

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,3 +9,5 @@ elm-make --yes elm/Main.elm elm/View.elm elm/Model.elm elm/AppStyles.elm elm/Tes
 dotnet publish -c Release -r ubuntu.14.04-x64
 
 # Push to CF
+cf login -a $api -u $username -p $password -o $organization -s $space &&\
+cf push -f $manifest
