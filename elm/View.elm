50b19c2708f8b57c8d9aa066586f8ac2258d7573
Sander Harrewijnen <sharrewijnen@live.com>
2019-01-31 14:08:53 +0100

    Removed all Elm Element based styling and moved to basic content generation and CSS instead. Also added some ITQ branding.

--- elm/View.elm
+++ elm/View.elm
@@ -5,50 +5,46 @@ import Model exposing (..)
 import TestResult.Model as TestResults exposing (..)
 import TestResult.View as TestResults exposing (..)
 
-import Html exposing (Html)
-import Element
-import Element.Attributes exposing (px, padding, spacing, alignRight, width, height, percent)
-
-
+import Html exposing (Html, h1, div, span, text, header, section)
+import Html.Attributes exposing (classList)
 
 -- VIEWS
 
 view : Model.Model -> Html Msg
 view model =
-    Element.layout (stylesheet model) <| (pageWrapper model)
-
-
-pageWrapper : Model.Model -> Element.Element DashboardStyles variation Msg
-pageWrapper model =
-    Element.column AppStyles.PageStyle
-        [ padding 20, spacing 40, height (percent 100) ]
-        [ headerArea model.lastReceived
-        , contentArea model.tests
+    div [ classList [("app", True)] ] [
+        header [ classList [("header", True)] ] [
+            lastReceivedTime model.lastReceived
         ]
+        , tests model.tests
+        , div [] []
+    ]
 
+lastReceivedTime : LastReceived -> Html Msg
+lastReceivedTime lastReceived =
+    span [ classList[("last-received-time", True)] ] [
+        text "Last updated "
+        , text (Maybe.withDefault "? seconds" (lastReceived.diffText))
+        , text " ago"
+    ]
 
-headerArea : LastReceived -> Element.Element DashboardStyles variation Msg
-headerArea lastReceived =
-    Element.row AppStyles.HeaderStyle
-        [ alignRight ]
-        [ Element.text
-            (case lastReceived.diffText of
-                Nothing -> "<unknown>"
-                Just string -> string)
-        , Element.text
-            (case lastReceived.diffText of
-                Nothing -> ""
-                Just string -> " ago")
-        ]
+tests : List TestResults.Model -> Html Msg
+tests results =
+    results
+        |> List.map (\r -> div [ classList [ ("test-container", True) ]] [ 
+            h1 [ classList [ ("test-name", True) ] ] [ text r.result.name ] 
+            , testOutput r.result.results
+             ])
+        |> section [ classList [("tests", True)]]
 
+testOutput : List SubTestResult -> Html Msg
+testOutput testResults =
+    testResults
+        |> List.map (\tr -> div [ classList [ ("test-result", True) ]] [ 
+            div [ classList [ ("result-name", True)] ] [ text tr.name ]
+            , div [ classList [ ("has-error", tr.error /= Nothing), ("result-error", True)] ] [ 
+                text (Maybe.withDefault "" (tr.error))
+                ]
+            ])
+        |> div [ classList [("test", True)]]
 
-contentArea : List TestResults.Model -> Element.Element DashboardStyles variation Msg
-contentArea testResults =
-    Element.wrappedRow AppStyles.ContentStyle
-        [ spacing 40 ]
-        (testResults
-            |> List.indexedMap (\index testResult ->
-                let
-                    testResultConfig = { toggleMsg = ToggleDetails index }
-                in
-                    TestResults.view testResultConfig testResult))
