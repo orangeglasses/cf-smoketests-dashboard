2c67160b6f52f53833a2fddd7a030f58433e635a
Sander Harrewijnen <sharrewijnen@live.com>
2020-11-30 21:59:06 +0100

    Better rendering and totals

--- dashboard/wwwroot/app/modules/render-snapshots.js
+++ dashboard/wwwroot/app/modules/render-snapshots.js
@@ -9,19 +9,30 @@ export default function renderSnapshots(snapshots) {
 function renderSingleSnapshot(snapShot, index) {
     const column = document.getElementById(`nodeSnapshot${index}`);
     clearNodes(column);
-
+    
+    var total = 0;
     snapShot.forEach(n => {
-        const node = document.createElement("div");
-        node.classList.add("single-node-stat");
-
-        const name = document.createElement("div");
-        name.innerText = `🧱 ${n.node}`;
-        node.appendChild(name);
-
-        const fps = document.createElement("div");
-        fps.innerText = `${n.fpsValue} 🎦`;
-        node.appendChild(fps);
+        const node = createRow(n.node, n.fpsValue);
+        total = total + n.fpsValue;
 
         column.appendChild(node);
     });
+    
+    const snapshotTotal = createRow("", total);
+    column.appendChild(snapshotTotal);
+}
+
+function createRow(title, value) {
+    const node = document.createElement("div");
+    node.classList.add("single-node-stat");
+
+    const name = document.createElement("div");
+    name.innerText = `${title}`;
+    node.appendChild(name);
+
+    const fps = document.createElement("div");
+    fps.innerText = `${value}`;
+    node.appendChild(fps);
+
+    return node;
 }
